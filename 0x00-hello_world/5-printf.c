447c07766c3d8527507207139a5a7dd719a5a506
Garvey17 <adebolaoluwatowo@gmail.com>
2022-12-11 09:00:31 -0800

    commit this too

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/* header of the file*/
+#include <stdio.h>
+
+/*where the execution begins*/
+int main()
+{
+
+	/*printing the message with the punction printf*/
+	printf("with proper grammar, but the outcome is a peice of art,");
+
+	return 0;
+}
