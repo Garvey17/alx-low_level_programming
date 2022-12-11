57ca0021a186b27769014509e29954092ccde384
Garvey17 <adebolaoluwatowo@gmail.com>
2022-12-11 08:35:27 -0800

    multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/* header file for input output function*/
+#include <stdio.h>
+
+/*where the execution begins*/
+int main()
+{
+
+	/*prints programming is like building a multilingual puzzle*/
+	puts("Programming is like building a multilingual puzzle");
+
+	return 0;
+}
